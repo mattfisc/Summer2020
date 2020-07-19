00cf239e0c051367cdd3c2bb84b666cc60f2cab6
Matthew Fischer <mattfisc@gmail.com>
2020-07-19 01:10:18 -0700

    time
    time format

--- basics_practice/src/App.java
+++ basics_practice/src/App.java
@@ -1,16 +1,26 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.LinkedList;
 
 public class App {
 
     public static void main(String[] args) {
-        LinkedList<String> cars = new LinkedList<String>();
-        cars.add("Volvo");
-        cars.add("BMW");
-        cars.add("Ford");
-        cars.add("Mazda");
-        System.out.println(cars);
+        LocalDate n = LocalDate.now();
+        System.out.println(n);
 
+        LocalTime time = LocalTime.now();
+        System.out.println(time);
+
+        LocalDateTime datetime = LocalDateTime.now();
+        System.out.println(datetime);
+
+        DateTimeFormatter formatTimeObj = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm::ss");
+
+        String formattedDate = datetime.format(formatTimeObj);
+        System.out.println("after format " + formattedDate);
 
     }
 }
