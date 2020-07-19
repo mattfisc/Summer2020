ffa9e65268dea30c55c01a3d604c215eab662515
Matthew Fischer <mattfisc@gmail.com>
2020-07-19 00:58:28 -0700

    update class name

---
+++ basics_practice/src/App.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class App {
+
+    public static void main(String[] args) {
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+
+
+    }
+}
